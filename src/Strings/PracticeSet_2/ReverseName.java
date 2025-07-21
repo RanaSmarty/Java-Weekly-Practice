a989076e86ca7842d4fe55d0b77ac7fe06f7a02d
Rana Smarty <ranasmarty00@gmail.com>
2025-07-22 00:11:16 +0600

    Update ReverseName to validate exactly two-word input (first and last name only)
    - Added logic to check that the full name contains exactly one space
    - Prevents invalid inputs like single names or more than two words
    - Improves user experience with clear error message

--- src/Strings/PracticeSet_2/ReverseName.java
+++ src/Strings/PracticeSet_2/ReverseName.java
@@ -21,14 +21,16 @@ public class ReverseName {
         Prompt the user to input their full name
         Read the user inputted full name and store it in a variable
 
-        Find the index of first space
-        IF the index is more than -1 THEN
-             Extract the last name using substring
-             Extract the first name using substring
+        IF there is a space in the input (i.e., firstSpace > -1)
+        AND
+        there is no second space in the input (i.e., only one space exists)
 
-             Print the results
-         ELSE
-            Print an invalid message!
+        THEN
+            Extract first name from index 0 to first space
+            Extract second name from (first space + 1) to end of string
+            Display: "Reversed name: [last name] [first name]"
+        ELSE
+            Display: "Invalid input! Please enter only first and last name."
         END IF
         Close the Scanner object to avoid resources leaks
     END
@@ -41,11 +43,11 @@ public class ReverseName {
 
         int firstSpace = fullName.indexOf(' ');
 
-        if (firstSpace > -1) {
+        if (firstSpace > -1 && fullName.indexOf(' ', firstSpace + 1) == -1) {
             String firstName = fullName.substring(0, firstSpace);
             String secondName = fullName.substring(firstSpace + 1);
             System.out.println();
-            System.out.println("Reversed name: " + secondName.concat(firstName));
+            System.out.println("Reversed name: " + secondName.concat(" ").concat(secondName));
             System.out.println();
         } else {
             System.out.println("Invalid! Try again...");
